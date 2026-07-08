8aa17a08140ef72cb9909c770bfedc92705e412f
Mahar GM <144817379+github-mahar@users.noreply.github.com>
2026-07-08 17:59:19 +0500

    feat: initialize Trusted Web Activity project structure and configuration

---
+++ app/src/main/java/online/ezzcode/my_portfolio/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package online.ezzcode.my_portfolio.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
